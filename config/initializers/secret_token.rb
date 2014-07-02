6acf5c7a6045bf74a48fb4112f5d30d77fa2c3dc
Robert Glaser <mail@robert-glaser.de>
2014-07-02 15:30:29 +0200

    remove hardcoded secret token

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -4,4 +4,4 @@
 # If you change this key, all old signed cookies will become invalid!
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
-Iqvoc::Moeko::Application.config.secret_token = '2604eba9fa63fcaa6aa38e4e2740ef481406b1aec3e9e879efd3b8b6c44440e9ab988a158d1e95fc59dda7a6ba60ef42276e4623ea1ffa06849f8e969739fe05'
+Iqvoc::Moeko::Application.config.secret_token = ENV['MEERTHES_SECRET_TOKEN']
